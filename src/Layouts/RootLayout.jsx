c3f066bbf6b8d8601f02afc2affaafdb2da891a1
Ranjit jana <jranjit367@gmail.com>
2024-08-23 13:56:35 +0530

    Add topbar section

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../Components/Navbar"
 import Footer from "../Components/Footer"
+import TopBar from "../Components/TopBar"
 
 const RootLayout = () => {
   return (
     <>
+    <TopBar/>
     <Navbar />
     <Outlet />
     <Footer />
