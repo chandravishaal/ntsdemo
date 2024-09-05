bb4ecc3b4fd580006d3d4481db04eadd1bdc0d4d
Abhishek Jha <126500719+Abhi192837@users.noreply.github.com>
2024-09-05 16:32:25 +0530

    404 page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-
 import { lazy, Suspense } from "react";
 import About from "./Pages/About";
+import Page404 from "./Pages/Page404";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -11,32 +11,27 @@ import Services from "./Pages/Services";
 
 const RootLayout = lazy(() => import("./Layouts/RootLayout"));
 const Dashboard = lazy(() => import("./Pages/Home"));
-const CryptoConverter =lazy(()=> import("./Pages/CryptoConverter"));
-const App = () => {
+const CryptoConverter = lazy(() => import("./Pages/CryptoConverter"));
 
+const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Dashboard />} />
-        <Route path="/About" element={<About/>}/>
-        <Route path="/Services" element={<Services/>}/>
-
-
-        <Route 
-          path="/crypto-converter"
-          element={<CryptoConverter/>}
-        />
+        <Route path="/About" element={<About />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/crypto-converter" element={<CryptoConverter />} />
+        {/* Catch-all route for undefined paths */}
+        <Route path="*" element={<Page404 />} />
       </Route>
-
-      )
-    );
-
+    )
+  );
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <RouterProvider router={router} />
     </Suspense>
-  )
+  );
 }
 
-export default App
+export default App;
