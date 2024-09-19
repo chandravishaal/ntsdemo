103303285a426d638696e4f6ea342ba3c47671e6
Iffat Nigar <iffatnigar18@gmail.com>
2024-09-20 04:10:35 +0530

    change the cryptocurrency page theme

--- src/Components/CryptocurrencyComponents/Bitcoin.jsx
+++ src/Components/CryptocurrencyComponents/Bitcoin.jsx
@@ -37,7 +37,7 @@ const Bitcoin = () => {
 
       {/* Content Section */}
       <div className="content-grid grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-10 mt-6">
-        {/* Chart and Price Section */}
+        {/* Chart and Price Section   */}
         <div className="chart-price-section p-6 bg-white rounded-lg shadow-md">
           {/* Timeframe Buttons with Space Between */}
           <div className="flex justify-between mb-4">
