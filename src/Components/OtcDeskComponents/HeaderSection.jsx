b36c8629ee4e31e7a4ff515b8a8f35efb75b6e62
Iffat Nigar <iffatnigar18@gmail.com>
2024-09-20 12:34:33 +0530

    fix error in header section of otc desk

--- src/Components/OtcDeskComponents/HeaderSection.jsx
+++ src/Components/OtcDeskComponents/HeaderSection.jsx
@@ -153,7 +153,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import '../../index.css';
 import background from '../../assets/Images/OtcDesk/hero_section_background.png';
 import bg from '../../assets/Images/OtcDesk/bg.png';
-import userIcon from '../../assets/Images/OtcDesk/userIcon.png';
+import userIcon from '../../assets/Images/OtcDesk/userIcon.png';  
 import emailIcon from '../../assets/Images/OtcDesk/emailIcon.png';
 import phoneIcon from '../../assets/Images/OtcDesk/phoneIcon.png';
 import dealIcon from '../../assets/Images/OtcDesk/dealIcon.png';
@@ -230,36 +230,28 @@ const HeaderSection = () => {
     >
       <div className="flex flex-col lg:flex-row items-center justify-between container mx-auto px-[4%] py-[6%] space-y-8 lg:space-y-[-3%] lg:space-x-[2%]">
         {/* Left Section (Text) */}
-        <div className={"w-full lg:w-1/2 text-left px-[-2%] lg:ml-[-2%]"}>
-          <h1 className={"text-3xl md:text-4xl jost-uniquifier font-semibold text-black mb-[6%] font-montserrat"}>
+        <div className={"left-section w-full lg:w-1/2 text-left px-[-2%] lg:ml-[-2%]"}>
+          <h1 className={"left-section-title text-3xl md:text-4xl font-semibold text-black mb-[6%] font-montserrat"}>
             Cryptocurrency OTC Over The Counter Exchange Desk Serving Globally
           </h1>
-          <p className={"text-lg lg:text-2xl xl:text-2xl jost-uniquifier text-black mb-[9%] font-century-gothic"}>
+          <p className={"left-section-text text-lg lg:text-2xl xl:text-2xl  text-black mb-[9%] font-century-gothic"}>
             Are you looking for an OTC (Over the Counter) Cryptocurrency Exchange Desk that can offer you several cryptocurrencies within seconds?
           </p>
 
           {/* Button Section */}
           <div className="flex justify-center lg:justify-start w-full">
-            <button className="bg-cyan-500 jost-uniquifier text-black px-[6%] py-[3%] rounded-lg font-medium text-lg font-century-gothic">
+            <button className="left-section-button bg-cyan-500  text-black px-[6%] py-[3%] rounded-lg font-medium text-lg font-century-gothic">
               Get Started Now
             </button>
           </div>
         </div>
 
-
-
         {/* Right Section (Form) */}
-        <div className={"relative  lg:w-[50%] flex items-center justify-center px-[4%]"}>
+        <div className={"relative form-container lg:w-[50%] flex items-center justify-center px-[4%]"}>
           {/* Form Background */}
           <div className="absolute inset-0 object-cover z-0  w-full h-[105%] lg:w-[100%] lg:h-[105%] p-[7%] lg:p-0 lg:ml-[10%]">
-            <img
-              src={bg}
-              className="w-full h-full object-cover rounded-3xl"
-            />
+            <img src={bg} className="w-full h-full object-cover rounded-3xl" />
           </div>
-
-
-
 
           {/* Form Container */}
           <div className="relative z-12 w-[95%] lg:w-[110%] mt-10 lg:mt-[2%] lg:-mr-[23%] max-w-lg lg:p-[6%] p-5 bg-transparent font-century-gothic">
@@ -352,51 +344,15 @@ const HeaderSection = () => {
               {/* Submit Button */}
               <button
                 type="submit"
-                className="w-[60%] bg-cyan-700 text-white p-3 rounded-lg font-medium text-lg lg:p-4 mx-[20%] font-century-gothic" >
+                className="w-[60%] bg-cyan-700 text-white p-3 rounded-lg font-medium text-lg lg:p-4 mx-[20%] font-century-gothic"
+              >
                 GET IN TOUCH
               </button>
             </form>
           </div>
-
-          {/* Company Name */}
-          <div className="relative">
-            <img
-              src={companyIcon}
-              className="absolute top-1/2 transform -translate-y-1/2 left-3 mt-[8.5%] lg:mt-[7.5%] w-30 h-30 ml-[-13.5%] lg:ml-[-11.5%] lg:w-[31%]"
-            />
-            <input
-              type="text"
-              placeholder="Company name"
-              className="w-full p-3 pl-12 rounded-lg bg-cyan-300 text-cyan-800 placeholder-cyan-800 focus:outline-none lg:p-4 lg:pl-[15%]"
-            />
-          </div>
-
-          {/* Comments */}
-          <div className="relative">
-            <img
-              src={commentsIcon}
-              className="absolute top-1/2 transform -translate-y-1/2 left-3 mt-[-3%] w-30 h-30 ml-[-12.5%] lg:ml-[-10.5%] lg:w-[31%] lg:mt-[-1.8%]"
-            />
-            <textarea
-              placeholder="Comments"
-              className="w-full p-3 pl-12 rounded-lg bg-cyan-300 text-cyan-800 placeholder-cyan-800 focus:outline-none lg:p-4 lg:pl-[15%]"
-              rows="2"
-            ></textarea>
-          </div>
-
-          {/* Submit Button */}
-          <button
-            type="submit"
-            className="w-[60%] bg-cyan-700 text-white p-3 rounded-lg font-medium text-lg lg:p-4 mx-[20%]"
-          >
-            GET IN TOUCH
-          </button>
         </div>
       </div>
     </div>
-
-
-
   );
 };
 
