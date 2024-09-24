f334110f75b7dd60aaaa0e0db93d501d7ff5b5e0
Aditya Panda <panda.aditya937@gmail.com>
2024-09-24 12:48:51 +0530

    changed the testimonial section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,13 @@ export default {
       'NorthStarsans': ['Nunito Sans', 'sans-serif'],
       'eras-bold': ['"Eras Bold ITC"', 'sans-serif'],
       'antipasto-pro': ['"Antipasto Pro"', 'sans-serif'],
-      // 'century-gothic': ['"Century Gothic"', 'sans-serif'],
       'montserrat': ['Montserrat', 'sans-serif'], // Added Montserrat
       'century-gothic': ['CustomFont', 'sans-serif'], // Added Century Gothic
     },
     extend: {
       colors: {
-        primaryCyan: '#00d1ff', // Add your custom color here
-        customRed: '#f87171',  // Another example of a custom color
+        primaryCyan: '#16D5FF', // Add your custom color here
+        secondaryGray: '#F3F4F6',  // Another example of a custom color
       },
       fontSize: {
         'xxs': '0.625rem', // 10px
