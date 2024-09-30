81e348985949b8ee0c0f9534a99b687d4dfdd682
Aditya Panda <panda.aditya937@gmail.com>
2024-09-30 13:21:42 +0530

    made changes on footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,6 +5,7 @@ import { MdEmail } from "react-icons/md";
 import { IoCall } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
+import PrimaryButton from "../Common/PrimaryButton";
 
 const Footer = () => {
   return (
@@ -12,75 +13,91 @@ const Footer = () => {
       <div className="">
         <Marquee gradient={true} gradientColor="black" loop={0} speed={60} pauseOnHover={true}
           style={{ color: 'white', padding: '4px', backgroundColor: 'black' }}>
-          Subscribe to our newsletter ● FAQ's - Your Questions Answered! ● Check Our ICO Calendar for Upcoming Events ● Explore NSM Logo - Your Gateway to Crypto ● Trade with Us - Buy & Sell 70+ Cryptocurrencies! ● Discover Products and Services at NSM! ● Learn More About Cryptocurrency Today! ● Buy and Sell Cryptocurrency 24/7 ● Get Started with North Star Metrics Now! ● Your Trusted Crypto Platform - North Star Metrics ● Enjoy Safe & Secure Transactions! ● Instant Order Processing - Trade Efficiently ● Volatility Protection Tools Available ● Stay Ahead: Market Cap Insights and Trends ● Join Over 10 Million Transactions!
+          <h1>Hello</h1>
         </Marquee>
 
-        <div className="my-14">
-          <div className="container mx-auto max-w-[1200px] flex flex-col md:flex-row gap-6 py-6 px-4 md:py-10 md:px-6">
+        <div className="mb-14 mt-10 font-century-gothic">
+          <div className="container mx-auto max-w-[1200px] flex flex-col md:flex-row gap-6 py-6 px-4 md:py-10 lg:px-0">
 
             {/* First flex container for the social section */}
             <div className="flex-[3] text-gray-500 font-semibold mb-6 md:mb-0">
-              <h2 className="text-lg md:text-xl font-semibold text-gray-700 mb-2">Social</h2>
-              <div className="flex space-x-3 md:space-x-4 my-3 mb-2">
+              <h2 className="text-lg font-semibold text-gray-700 mb-2">Social</h2>
+              <div className="flex space-x-3 md:space-x-4 my-3 lg:mb-10 mb-5">
                 <FaTwitter className="text-lg transition-colors duration-300 hover:text-black md:text-xl" />
                 <FaLinkedinIn className="text-lg transition-colors duration-300 hover:text-black md:text-xl" />
                 <FaInstagram className="text-lg transition-colors duration-300 hover:text-black md:text-xl" />
-                <FaDribbble className="text-lg transition-colors duration-300 hover:text-black md:text-xl" />
               </div>
-              <p className="text-base transition-colors duration-300 hover:text-black">
+              <p className="text-sm transition-colors duration-300 mb-2 hover:text-black">
                 <FaLocationDot className="inline mr-2" />
                 7th Floor, Yamuna Building, 86, Golaghata Rd, Dakshindari, Kolkata - 700048
               </p>
-              <p className="text-base transition-colors duration-500 hover:text-black">
+              <p className="text-sm transition-colors mb-2 duration-500 hover:text-black">
                 <IoCall className="inline mr-2" />
                 +91 8981037010
               </p>
-              <p className="text-base transition-colors duration-500 hover:text-black">
+              <p className="text-sm transition-colors duration-500 hover:text-black">
                 <MdEmail className="inline mr-2" />
                 contact@ntsmetrics.com
               </p>
             </div>
 
             {/* 2nd flex for the rest*/}
-            <div className="flex-[7] flex flex-col md:flex-row md:justify-between gap-14">
+            <div className="flex-[7] flex flex-col md:flex-row md:justify-between lg:gap-14 gap-7">
               {/* Our services section */}
               <div className="text-gray-500 font-semibold md:ml-52 md:w-4/12">
-                <h3 className="text-lg md:text-xl font-semibold text-gray-700 mb-2">Our Services</h3>
-                <ul className="space-y-1">
-                  <li><Link to="/" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Exchange Services</Link></li>
-                  <li><Link to="/" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Ticker</Link></li>
-                  <li><Link to="/blogs" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Blog</Link></li>
-                  <li><Link to="/features" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Features</Link></li>
-                  <li><Link to="/roadmap" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Roadmap</Link></li>
+                <h3 className="text-lg font-semibold text-gray-700 mb-3">Services</h3>
+                <ul className="space-y-2">
+                  <li><Link to="/" className="transition-colors duration-300 hover:text-black text-sm">Exchange Services</Link></li>
+                  <li><Link to="/" className="transition-colors duration-300 hover:text-black text-sm">Ticker</Link></li>
+                  <li><Link to="/blogs" className="transition-colors duration-300 hover:text-black text-sm">Blog</Link></li>
+                  <li><Link to="/features" className="transition-colors duration-300 hover:text-black text-sm">Features</Link></li>
+                  <li><Link to="/roadmap" className="transition-colors duration-300 hover:text-black text-sm">Roadmap</Link></li>
                 </ul>
               </div>
 
               {/* Products section */}
               <div className="text-gray-500 font-semibold md:ml-5 md:w-4/12">
-                <h3 className="text-lg md:text-xl font-semibold text-gray-700 mb-2">Products</h3>
-                <ul className="space-y-1">
-                  <li><Link to="/about" className="transition-colors duration-300 hover:text-black text-sm md:text-base">About Us</Link></li>
-                  <li><Link to="/contact" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Contact Us</Link></li>
-                  <li><Link to="/" className="transition-colors duration-300 hover:text-black text-sm md:text-base">AML & KYC</Link></li>
-                  <li><Link to="/terms" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Terms</Link></li>
-                  <li><Link to="/privacy-policy" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Privacy Policy</Link></li>
+                <h3 className="text-lg font-semibold text-gray-700 mb-3">Products</h3>
+                <ul className="space-y-2">
+                  <li><Link to="/about" className="transition-colors duration-300 hover:text-black text-sm">About Us</Link></li>
+                  <li><Link to="/contact" className="transition-colors duration-300 hover:text-black text-sm">Contact Us</Link></li>
+                  <li><Link to="/" className="transition-colors duration-300 hover:text-black text-sm">AML & KYC</Link></li>
+                  <li><Link to="/terms" className="transition-colors duration-300 hover:text-black text-sm">Terms</Link></li>
+                  <li><Link to="/privacy-policy" className="transition-colors duration-300 hover:text-black text-sm">Privacy Policy</Link></li>
                 </ul>
               </div>
 
               {/* Resources section */}
               <div className="text-gray-500 font-semibold md:ml-5 md:w-4/12">
-                <h3 className="text-lg md:text-xl font-semibold text-gray-700 mb-2">Resources</h3>
-                <ul className="space-y-1">
-                  <li><Link to="/about" className="transition-colors duration-300 hover:text-black text-sm md:text-base">About Us</Link></li>
-                  <li><Link to="/contact" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Contact Us</Link></li>
-                  <li><Link to="/" className="transition-colors duration-300 hover:text-black text-sm md:text-base">AML & KYC</Link></li>
-                  <li><Link to="/terms" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Terms & Condition</Link></li>
-                  <li><Link to="/privacy-policy" className="transition-colors duration-300 hover:text-black text-sm md:text-base">Privacy Policy</Link></li>
+                <h3 className="text-lg font-semibold text-gray-700 mb-3">Resources</h3>
+                <ul className="space-y-2">
+                  <li><Link to="/about" className="transition-colors duration-300 hover:text-black text-sm">About Us</Link></li>
+                  <li><Link to="/contact" className="transition-colors duration-300 hover:text-black text-sm">Contact Us</Link></li>
+                  <li><Link to="/" className="transition-colors duration-300 hover:text-black text-sm">AML & KYC</Link></li>
+                  <li><Link to="/terms" className="transition-colors duration-300 hover:text-black text-sm">Terms & Condition</Link></li>
+                  <li><Link to="/privacy-policy" className="transition-colors duration-300 hover:text-black text-sm">Privacy Policy</Link></li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
+
+        <div className="w-full h-[0.5px] bg-gray-200"></div>
+
+        <div className="flex items-center justify-between container font-century-gothic mx-auto my-10">
+            <div className="flex flex-col items-start gap-2">
+                <h1 className="text-lg text-gray-600 font-semibold">Interested to stay up-to-date with cryptocurrencies?</h1>
+                <p className="text-sm">Get the latest crypto news, updates, and reports by subscribing to our free newsletter.</p>
+            </div>
+
+            <div className="flex items-center gap-5">
+              <input type="text" placeholder="Enter your email address" className="p-2 rounded-2xl border-2 placeholder:text-sm text-sm w-[15rem]" />
+              <PrimaryButton title="Subscribe" />
+            </div>  
+        </div>
+
+        <div className="w-full h-[0.5px] bg-gray-200"></div>
+
 
         {/* Footer bottom */}
         <div className="text-center font-semibold text-gray-500 p-2 my-8 text-sm">
