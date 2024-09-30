e329fc0fde58650140c0c697eb34bb5c3d925e1a
chandravishaal <chandravishal9911.com>
2024-09-30 20:32:19 +0530

    redesigned the  press release

--- src/Components/AboutComponents/PressRoom.jsx
+++ src/Components/AboutComponents/PressRoom.jsx
@@ -5,13 +5,19 @@ import Image1 from '../../assets/Images/icon1.jpg';
 import Image2 from '../../assets/Images/icon2.jpg';
 import Image3 from '../../assets/Images/icon3.jpg';
 import Image4 from '../../assets/Images/nts-removebg-preview.png';
-import { Link, redirect } from 'react-router-dom';
+// import { Link, redirect } from 'react-router-dom';
+import { CiCalendarDate } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa6";
+
+
 
 gsap.registerPlugin(ScrollTrigger);
 
 function PressRoom() {
   const cardsData = [
     {
+      
       imageSrc: "https://miro.medium.com/v2/resize:fit:720/format:webp/1*L7tpgNK6Rg_09CBXJIp3sQ.jpeg",
       title: 'Buy and Sell Crypto: Functions and Benefits of Binance Coin',
       description:
@@ -92,30 +98,82 @@ function PressRoom() {
     );
   }, []);
 
+  // const PressCard = ({ imageSrc, title, description, logoSrc, index, redirectLink }) => {
+  //   return (
+  //     <div
+  //       ref={(el) => (cardRefs.current[index] = el)} // Assigning the ref to each card
+  //       className="bg-cyan-50 rounded-lg h-[33rem] overflow-hidden"
+  //     >
+  //       <img src={imageSrc} alt="Press Image" className="rounded-t-lg object-cover mb-3 h-[200px] w-full" />
+  //       <div className='p-4'>
+  //         <h4 className="text-lg md:text-xl font-bold mb-2 font-montserrat">{title}</h4>
+  //         <p className="text-gray-500 mb-4 text-sm md:text-base line-clamp-6 font-century-gothic">
+  //           {description}
+  //         </p>
+  //         <div className='flex flex-wrap items-center justify-end'>
+            
+  //           <div className="flex flex-wrap justify-end mt-4">
+            
+  //             <a href={redirectLink} className="bg-primaryCyan text-black font-bold py-1 px-4 rounded-full text-xs md:text-sm font-century-gothic">Read more</a>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // };
+
   const PressCard = ({ imageSrc, title, description, logoSrc, index, redirectLink }) => {
     return (
       <div
         ref={(el) => (cardRefs.current[index] = el)} // Assigning the ref to each card
-        className="bg-cyan-50 rounded-lg h-[33rem] overflow-hidden"
+        className="bg-white flex flex-col items-start gap-4 rounded-2xl shadow-md outline outline-[0.2px] outline-gray-300 p-4"
       >
-        <img src={imageSrc} alt="Press Image" className="rounded-t-lg object-cover mb-3 h-[200px] w-full" />
-        <div className='p-4'>
-          <h4 className="text-lg md:text-xl font-bold mb-2 font-montserrat">{title}</h4>
-          <p className="text-gray-500 mb-4 text-sm md:text-base line-clamp-6 font-century-gothic">
-            {description}
-          </p>
-          <div className='flex flex-wrap items-center justify-end'>
-            
-            <div className="flex flex-wrap justify-end mt-4">
-            
-              <a href={redirectLink} className="bg-primaryCyan text-black font-bold py-1 px-4 rounded-full text-xs md:text-sm font-century-gothic">Read more</a>
+        <h1 className="text-lg font-semibold px-1 font-century-gothic">
+          {title}
+        </h1>
+  
+        <div className="flex items-center px-1 gap-3">
+          {/* date */}
+          <div className="rounded-xl flex items-center gap-2 bg-[#F3F4F6] p-1 px-2 text-sm outline outline-[0.2px] outline-gray-300">
+            <span className="text-primaryCyan">
+              <CiCalendarDate size={15} />
+            </span>
+            <h1>24 Sept 2024</h1>
+          </div>
+          {/* likes  */}
+          <div className="rounded-xl flex items-center gap-2 bg-[#F3F4F6] p-1 px-2 text-sm outline outline-[0.2px] outline-gray-300">
+            <span className="text-primaryCyan">
+              <FaHeart />
+            </span>
+            <h1>10k</h1>
+          </div>
+        </div>
+  
+        <h1 className="line-clamp-2 text-sm px-1 font-montserrat">
+          {description}
+        </h1>
+  
+        <div className="relative w-full">
+          <img
+            src={imageSrc}
+            alt="Press Image"
+            className="w-full object-cover h-[300px] rounded-xl z-40"
+          />
+          <div className="bg-white pr-2 pb-2 pt-1 absolute z-50 top-0 left-0 rounded-br-xl">
+            <div className="bg-white group hover:bg-[#F3F4F6] transition-all duration-300 p-3 outline outline-[2px] rounded-xl outline-gray-400">
+              <a href={redirectLink} className="flex items-center">
+                Read Article
+                <span className="group-hover:translate-x-1 transition-transform duration-300 ml-2 text-primaryCyan">
+                  <FaArrowRight />
+                </span>
+              </a>
             </div>
           </div>
         </div>
       </div>
     );
   };
-
+  
   return (
     <section className="bg-white container mx-auto mb-24 mt-10">
       <div className="text-center font-Antipasto pro">
