5000b1e4a6165e4cb8c46b5e29b73e2e380747e7
Aditya Panda <panda.aditya937@gmail.com>
2024-10-02 11:50:14 +0530

    fixed some minor changes

--- src/Components/DesktopFooter.jsx
+++ src/Components/DesktopFooter.jsx
@@ -230,12 +230,12 @@ import {
           <div className="w-full h-[0.5px] bg-gray-200"></div>
   
           {/* Footer bottom */}
-          <div className="text-center font-semibold text-gray-500 p-2 my-8 text-sm">
+          <div className="text-center font-semibold text-gray-500 p-2 mt-8 text-sm">
             &copy; 2024 NORTH STAR METRICS, All Rights Reserved.
           </div>
-          <div className=" font-century-gothic font-semi
-          bold text-gray-500 p-2 my-8 text-sm">
-          <a href="/disclaimer" className="text-gray-700 lg:hover:text-primaryCyan">DISCLAIMER:</a> Trading in cryptocurrencies is subject to market & legal risks. Prices in one country may differ from prices in other countries. North Star Metrics does not guarantee any returns. Use North Star Metrics's services at your own risk.
+          <div className=" font-century-gothic container mx-auto text-center font-semi
+          bold text-gray-500 p-2 my-2 text-sm">
+          <Link to="/disclaimer" className="text-gray-500 font-semibold"><span className="lg:hover:text-primaryCyan">DISCLAIMER </span>: <span>Trading in cryptocurrencies is subject to market & legal risks. Prices in one country may differ from prices in other countries. North Star Metrics does not guarantee any returns. Use North Star Metrics&apso;s services at your own risk</span></Link>
           </div>
       </div>
       </>
