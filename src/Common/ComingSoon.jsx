74104b084836990bbf0e7652db605ed72cb7fb8e
Aditya Panda <panda.aditya937@gmail.com>
2024-10-08 14:12:06 +0530

    improved animation code

--- src/Common/ComingSoon.jsx
+++ src/Common/ComingSoon.jsx
@@ -1,26 +1,26 @@
-// import { useScramble } from 'use-scramble';
+import { useScramble } from 'use-scramble';
 
-// const ComingSoon = () => {
+const ComingSoon = () => {
 
-//     const { ref } = useScramble({
-//         text: 'Coming Soon',
-//         speed: 0.5,
-//         tick: 1,
-//         step: 1,
-//         scramble: 4,
-//         seed: 0,
-//       });
+    const { ref } = useScramble({
+        text: 'Coming Soon',
+        speed: 0.5,
+        tick: 1,
+        step: 1,
+        scramble: 4,
+        seed: 0,
+      });
 
-//   return (
-//     <section className='lg:h-screen flex items-center justify-center'>
-//         <div>
-//             <h1 ref={ref} className='text-5xl lg:text-7xl font-montserrat font-bold text-center mt-72 md:mt-96 lg:mt-0 animate-gradient' />
-//         </div>
-//     </section>
-//   )
-// }
+  return (
+    <section className='lg:h-screen flex items-center justify-center'>
+        <div>
+            <h1 ref={ref} className='text-5xl lg:text-7xl font-montserrat font-bold text-center mt-72 md:mt-96 lg:mt-0 animate-gradient' />
+        </div>
+    </section>
+  )
+}
 
-// export default ComingSoon
+export default ComingSoon
 
 
 
